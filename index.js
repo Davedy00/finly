198aee55ee8b81b976fb8fb23824d91c1e3457c8
KENNE GERI <kennegeri8@gmail.com>
2024-12-05 12:09:29 +0100

    1st commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+res.send('Hello From Node.js');
+});
+app.get('/contact', (req, res) => {
+    res.send('The Contact Page');
+});
+app.get('/about', (req, res) => {
+res.send('The About Page');
+});
+app.get('*', (req, res) => {
+    res.status(404).send('Not Found');
+});
+const PORT = 3000;
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
+});
