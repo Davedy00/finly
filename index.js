989180861a7bb87fc6801b5c09e430a5f0a50fe8
KENNE GERI <kennegeri8@gmail.com>
2024-12-25 23:55:53 +0100

    running the routes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+// const userRouter = require('./routes/user.route');
 const userRouter = require('./routes/user.route');
+const app = express();
 
 require('dotenv').config();
 require('./libs/dbConnect');
 
-const app = express();
+// const app = express();
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -13,15 +15,15 @@ app.use(morgan('dev'));
 app.get('/', (req, res) => {
     res.render('index', { message: 'Hello From Node.js' });
 });
-app.get('/contact', (req, res) => {
-    res.render('index', { message: 'The Contact Page' });
-});
-app.get('/about', (req, res) => {
-    res.render('index', { message: 'The About Page' });
-});
-app.get('*', (req, res) => {
-    res.status(404).render('index', { message: 'Not Found' });
-});
+// app.get('/contact', (req, res) => {
+//     res.render('index', { message: 'The Contact Page' });
+// });
+// app.get('/about', (req, res) => {
+//     res.render('index', { message: 'The About Page' });
+// });
+// app.get('*', (req, res) => {
+//     res.status(404).render('index', { message: 'Not Found' });
+// });
 const PORT = 3000;
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`);
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const {
-getUser,
-createUser,
-deleteUser,
+    getUser,
+    createUser,
+    deleteUser,
 } = require('../controllers/user.controller');
-router.get('/', getUser);
-router.get('/create', createUser);
-router.get('/delete', deleteUser);
+
+router.get('/', getUser);          // This route handles GET requests to /users
+router.get('/create', createUser); // This route handles GET requests to /users/create
+router.get('/delete', deleteUser); // This route handles GET requests to /users/delete
+
 module.exports = router;
