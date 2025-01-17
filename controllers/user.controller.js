331eaf983825f5e9486690812623193faa9182dc
KENNE GERI <kennegeri8@gmail.com>
2025-01-17 23:50:45 +0100

    recreating user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,22 +1,21 @@
 const User = require('../libs/models/user.model');
-const signup = async (req, res) => {
-    const { email, password } = req.body;
-    const query = { email };
-    const existingUser = await User.findOne(query);
-    if (existingUser) {
-    // Email already exists
-    res.redirect('/signup');
-    } else {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = {
-    email,
-    password: hashedPassword,
+const createUser = async (req, res) => {
+    await User.create({
+    email: 'nathan@mail.com',
+    password: 'password',
+    });
+    res.render('user', { message: 'User Created', user: null });
     };
-    const result = await User.create(user);
-    req.session.userId = result._id;
-    res.redirect('/dashboard');
-    }
+    const getUser = async (req, res) => {
+    const user = await User.findOne({ email: 'nathan@mail.com' });
+    res.render('user', { message: 'User Retrieved', user: user });
+    };
+    const deleteUser = async (req, res) => {
+    await User.findOneAndDelete({ email: 'nathan@mail.com' });
+    res.render('user', { message: 'User Deleted', user: null });
     };
     module.exports = {
-    signup,
+    getUser,
+    createUser,
+    deleteUser,
     };
